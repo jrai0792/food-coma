40bec801e63997c9208554d05a9ab0a6f04d0be1
Jyoti <jyotikiran12@gmail.com>
2021-01-10 07:05:31 +0530

    Add styles for signup and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'categories#index'
-  resources :categories, :only => [:new, :create, :destroy]
+  resources :categories, :only => [:new, :create, :destroy, :index, :show]
   resources :articles, :only => [:index, :new, :create, :show, :destroy]
   resources :article_categories, :only => [:new]
 
