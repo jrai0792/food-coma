78c6d872b6a5bcb24457e8953f3f9b8dc761d17e
Jyoti <jyotikiran12@gmail.com>
2021-01-12 23:10:41 +0530

    Fixed linters error

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,15 +13,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   CarrierWave.configure do |config|
     config.cache_storage = :file
-   end
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  
   def extension_whitelist
     %w[jpg jpeg gif png]
   end
-
 end
